16bfb9f98bbcd9329451cd164a1c3a804954428a
RY4N <ryan.is.nomore7@gmail.com>
2025-07-17 12:09:20 +0600

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+// Kaza - Advanced Lavalink wrapper with intelligent multi-platform search
+export { Kazagumo } from './Kazagumo';
+export { Kazagumo as Kaza } from './Kazagumo'; // Alias for the new name
+
+// Core managers
+export { KazagumoPlayer } from './Managers/KazagumoPlayer';
+export { KazagumoQueue } from './Managers/KazagumoQueue';
+export { EnhancedSearchManager } from './Managers/SearchManager';
+
+// Enhanced utilities
+export { URLParser } from './Utils/URLParser';
+export { ErrorHandler, ErrorCode } from './Utils/ErrorHandler';
+export { PluginConfig } from './config/PluginConfig';
+
+// Plugins
+export { PlayerMoved } from './Plugins/PlayerMoved';
+
+// Types
+export * from './types';
+
+// Version info
+export const version = '3.3.0';
+export const libraryName = 'Kaza';
+
+// Default export for convenience
+import { Kazagumo } from './Kazagumo';
+import { KazagumoPlayer } from './Managers/KazagumoPlayer';
+import { KazagumoQueue } from './Managers/KazagumoQueue';
+import { EnhancedSearchManager } from './Managers/SearchManager';
+import { URLParser } from './Utils/URLParser';
+import { ErrorHandler, ErrorCode } from './Utils/ErrorHandler';
+import { PluginConfig } from './config/PluginConfig';
+import { PlayerMoved } from './Plugins/PlayerMoved';
+
+export default { 
+    Kazagumo, 
+    Kaza: Kazagumo, 
+    KazagumoPlayer, 
+    KazagumoQueue, 
+    EnhancedSearchManager,
+    URLParser, 
+    ErrorHandler, 
+    ErrorCode,
+    PluginConfig,
+    PlayerMoved,
+    version,
+    libraryName
+};
